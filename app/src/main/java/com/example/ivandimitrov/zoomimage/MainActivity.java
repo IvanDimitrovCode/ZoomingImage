e85cbc34cb34a59aa33597a2d1fce42942ff9b0f
Ivan Dimitrov <Ivan Dimitrov@nemetschek.bg>
2017-02-27 16:07:23 +0200

    1.Added dragging with both fingers

--- app/src/main/java/com/example/ivandimitrov/zoomimage/MainActivity.java
+++ app/src/main/java/com/example/ivandimitrov/zoomimage/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.ivandimitrov.zoomimage;
 
-
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.os.Bundle;
@@ -10,9 +9,9 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int STATE_IDLE = 0;
-    private static final int STATE_DRAG = 1;
-    private static final int STATE_ZOOM = 2;
+    private static final int STATE_IDLE        = 0;
+    private static final int STATE_DRAG        = 1;
+    private static final int STATE_ZOOM_ROTATE = 2;
 
     private ImageView mImage;
     private Matrix mMatrix       = new Matrix();
@@ -21,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private PointF mMidPoint     = new PointF();
     private float  mOldDist      = 1;
     private int    mCurrentState = STATE_IDLE;
+    private float mSecondX, mSecondY, mFirstX, mFirstY;
+    private float mAngle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,27 +47,36 @@ public class MainActivity extends AppCompatActivity {
                         if (mOldDist > 10) {
                             mSavedMatrix.set(mMatrix);
                             midPoint(mMidPoint, event);
-                            mCurrentState = STATE_ZOOM;
+                            mFirstX = event.getX(0);
+                            mFirstY = event.getY(0);
+                            mSecondY = event.getY(1);
+                            mSecondX = event.getX(1);
+                            mCurrentState = STATE_ZOOM_ROTATE;
                         }
-                        break;
-
-                    case MotionEvent.ACTION_UP:
-                    case MotionEvent.ACTION_POINTER_UP:
-                        mCurrentState = STATE_IDLE;
                         break;
 
                     case MotionEvent.ACTION_MOVE:
                         if (mCurrentState == STATE_DRAG) {
                             mMatrix.set(mSavedMatrix);
                             mMatrix.postTranslate(event.getX() - mStartPoint.x, event.getY() - mStartPoint.y);
-                        } else if (mCurrentState == STATE_ZOOM) {
+                        } else if (mCurrentState == STATE_ZOOM_ROTATE) {
+                            System.out.println(mAngle);
+                            System.out.println((mAngle * -1));
+
                             float newDist = distance(event);
+                            mAngle = angleBetweenLines(event);
                             if (newDist > 10) {
                                 mMatrix.set(mSavedMatrix);
                                 float scale = newDist / mOldDist;
+                                mMatrix.postTranslate(event.getX() - mStartPoint.x, event.getY() - mStartPoint.y);
+                                mMatrix.preRotate(mAngle, (mFirstX + mSecondX) / 2, (mFirstY + mSecondY) / 2);
                                 mMatrix.postScale(scale, scale, mMidPoint.x, mMidPoint.y);
                             }
                         }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_POINTER_UP:
+                        mCurrentState = STATE_IDLE;
                         break;
                 }
                 view.setImageMatrix(mMatrix);
@@ -84,7 +94,14 @@ public class MainActivity extends AppCompatActivity {
                 float y = event.getY(0) + event.getY(1);
                 point.set(x / 2, y / 2);
             }
-        });
 
+            private float angleBetweenLines(MotionEvent event) {
+                float angle1 = (float) Math.atan2((mSecondY - mFirstY), (mSecondX - mFirstX));
+                float angle2 = (float) Math.atan2((event.getY(1) - event.getY(0)), (event.getX(1) - event.getX(0)));
+
+                float angle = ((float) Math.toDegrees(angle1 - angle2)) % 360;
+                return (angle * -1);
+            }
+        });
     }
 }
